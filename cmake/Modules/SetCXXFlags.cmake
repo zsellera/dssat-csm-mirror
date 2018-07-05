d609e69e3cbb34edad55b034ebe35a6c6a777e51
Fabio Oliveira <fabioaugusto@protonmail.com>
2018-07-05 08:36:15 -0300

    Added new structure for FlexibleIO in CMake files

--- cmake/Modules/SetCXXFlags.cmake
+++ cmake/Modules/SetCXXFlags.cmake
@@ -1,4 +1,44 @@
+####################################################################
+INCLUDE(${CMAKE_MODULE_PATH}/SetCompileFlag.cmake)
+
+# Make sure the build type is uppercase
+STRING(TOUPPER "${CMAKE_BUILD_TYPE}" BT)
+
+IF(BT STREQUAL "RELEASE")
+    SET(CMAKE_BUILD_TYPE RELEASE CACHE STRING
+      "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
+      FORCE)
+ELSEIF(BT STREQUAL "DEBUG")
+    SET (CMAKE_BUILD_TYPE DEBUG CACHE STRING
+      "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
+      FORCE)
+ELSEIF(BT STREQUAL "TESTING")
+    SET (CMAKE_BUILD_TYPE TESTING CACHE STRING
+      "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
+      FORCE)
+ELSEIF(NOT BT)
+    SET(CMAKE_BUILD_TYPE DEBUG CACHE STRING
+      "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
+      FORCE)
+    MESSAGE(STATUS "CMAKE_BUILD_TYPE not given, defaulting to DEBUG")
+ELSE()
+    MESSAGE(FATAL_ERROR "CMAKE_BUILD_TYPE not valid, choices are DEBUG, RELEASE, or TESTING")
+ENDIF(BT STREQUAL "RELEASE")
+
 ######################################################
 # Determine and set the CXX compiler flags we want 
 ######################################################
-SET(CMAKE_CXX_FLAGS "-std=c++0x -std=gnu++0x -c -fPIC" )
+SET_COMPILE_FLAG(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}"
+                 CXX "/EHsc"           # MSVC
+                     "/std:c++latest"  # MSVC
+                     "-std=c++0x"      # GNU
+                     "-std=gnu++0x"    # GNU
+                     " -c"             # GNU
+                     "-fPIC"           # GNU  
+                )
+
+SET_COMPILE_FLAG(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}"
+                 CXX  "/MTd"          # MSVC
+                      "-g"            # GNU
+
+		            )
